0700baf946858f1ad531526f08172204107c6c6b
Anna Hartman <126839762+annapachecohartman@users.noreply.github.com>
2024-01-24 13:56:02 -0600

    Index page, form partial

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,14 @@
 class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to books_path
+    else
+      render 'new'
+    end
+  end
+
+
 end
